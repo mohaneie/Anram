973dd7f9df8f10620b021246afcb249862ef3c12
mohaneie <mohaneie88@gmail.com>
2018-10-21 17:48:49 +0530

    leave mail is done here

--- Routes/leave.js
+++ Routes/leave.js
@@ -6,53 +6,53 @@ const User = require('../Models/leaves');
 module.exports = function (app) {
 
     app.post('/mail', (req, res) => {
-        
+
         const data = req.body;
+        const employeeId =req.body.employeeId;
+        const from = req.body.from;
+        const to = req.body.to;
+        const description = req.body.description;
         console.log(data);
 
         const user = new User(data);
         console.log(user);
         user.save()
-        .then((result) => {
-            res.json(result)
+            .then((result) => {
+                res.json(result)
+            })
+            .catch((error) => {
+                res.status(400).json({ message: error.message })
+            })
+
+        const transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
+            auth: {
+                user: 'mohanbabu1013@gmail.com',
+                pass: 'welcome@1'
+            }
         })
-        .catch((error) => {
-            res.status(400).json({message: error.message})
+
+        var mailOptions = {
+            from: 'mohanbabu1013@gmail.com',
+            to: 'mohaneie88@gmail.com',
+            subject: 'Hello',
+            text: 'Hello buddy',
+            html: `<b>${"Employeecode:"+ " "+employeeId}<br>${"From:"+" "+from}<br>${"To:"+" " +to}<br>${"Description:"+ " " +description} <button>CLICK-TO-APPROVE</b><br><button>CLICK-TO-CANCEL`
+
+        };
+
+        transporter.sendMail(mailOptions, (err, info) => {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log(info)
+                res.json(info);
+            }
+            transporter.close()
+
         })
-
-    
-
-       
-    
-        // const transporter = nodemailer.createTransport({
-        //     host: 'smtp.gmail.com',
-        //     port: 465,
-        //     secure: true,
-        //     auth: {
-        //         user: 'mohanbabu1013@gmail.com',
-        //         pass: 'welcome@1'
-        //     }
-        // })
-
-        // var mailOptions = {
-        //     from: 'mohanbabu1013@gmail.com',
-        //     to: 'mohaneie88@gmail.com',
-        //     subject: 'Hello',
-        //     text: 'Hello buddy',
-        //     html: `<b>${employeeId}<br> ${leavefromdate}<br> ${leaveuptodate}<br> ${description}<br> ${appliedon} <br> <button>CLICK-TO-APPROVE</b><br><button>CLICK-TO-CANCEL`
-                  
-        // };
-
-        // transporter.sendMail(mailOptions, (err, info) => {
-        //     if (err) {
-        //         console.log(err)
-        //     } else {
-        //         console.log(info)
-        //         res.json(info);
-        //     }
-        //     transporter.close()
-
-        // })
 
     })
 
@@ -60,14 +60,14 @@ module.exports = function (app) {
 
         const id = req.params.id;
         const data = req.body;
-        User.findByIdAndUpdate(id, data, {new: true})
-        .then((result) => {
-            res.json(result);
-        })
+        User.findByIdAndUpdate(id, data, { new: true })
+            .then((result) => {
+                res.json(result);
+            })
 
-        .catch((error) => {
-            res.status(400).json({message: error.message});
-        })
-        
+            .catch((error) => {
+                res.status(400).json({ message: error.message });
+            })
+
     })
 }
