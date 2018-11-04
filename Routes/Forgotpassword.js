22a99525aeb3ac3da73ccb5ff8278ad3ed8d6e56
mohaneie <mohaneie88@gmail.com>
2018-11-04 16:26:46 +0530

    reset route is completed

--- Routes/Forgotpassword.js
+++ Routes/Forgotpassword.js
@@ -1,24 +1,54 @@
 'use strict';
 
 const User = require('../Models/user');
+const myfun = require('../helper/otp');
+const passwordserv = require('../helper/password');
 
 module.exports = function (app) {
 
-    app.post('/forgot', (req, res) => {
-
+    app.post('/forgot', async (req, res, next) => {
         const { Email } = req.body;
-        User.findOne({ Email })
-            .then((data) => {
-                if (data) {
-                    const x = Math.floor((Math.random() * 10000))
-                    res.json(x);
-                    return UserSchema.findOneAndUpdate({ Email }, { Otp: x }, { new: true });
+        try {
+            const data = await User.findOne({Email});
+            if (!data) {
+                const error = new Error('Mail Doesnt exist')
+                return next(error);
+            }
+            const x = (Math.random() * Date.now() * 1000).toString().slice(0, 8);
+            const result = await User.findOneAndUpdate({ Email }, { Otp: x }, { new: true });
+            res.json(result);
+                const r = result.Otp;
+                console.log(r);
+                myfun(r);
+        }
 
-                }
-                res.json(data);
-            })
+          catch(error) {
+              next(error);
+          }
+    })
 
-            .catch((next) => {
-            })
+    /**resetPassword */
+
+    app.post('/reset', async (req, res, next) => {
+
+        const { Otp, Password } = req.body;
+        try {
+
+            const data = await User.findOne({ Otp });
+            if (!data) {
+                const error = new Error('OTP is not matching');
+                res.status(400).json({ message: error.message });
+            }
+            const hash = await passwordserv.hash(Password);
+            req.body.Password = hash;
+            const info = req.body;
+            const result = await User.findOneAndUpdate({ Otp }, info, { new: true });
+            res.json(result);
+
+        }
+        catch (error) {
+            next(error);
+        }
+
     })
 }
---
+++ helper/otp.js
@@ -0,0 +1,34 @@
+'use strict';
+const nodemailer = require('nodemailer');
+
+
+function myfun(r) {
+    var transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true, //true --> will use ssl
+        auth: {
+            user: 'mohanbabu1013@gmail.com',
+            pass: 'welcome@1',
+        }
+    });
+
+    var mailOptions = {
+        from: 'mohanbabu1013@gmail.com',
+        to: 'mohaneie88@gmail.com',
+        subject: 'Hello',
+        text: 'Hello world',
+        html: `${'OTP:' + r}`,
+    };
+    
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Message sent: ' + info.response);
+        }
+        transporter.close();
+    });
+}
+
+module.exports = myfun;
