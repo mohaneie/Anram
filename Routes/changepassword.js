2da4c552b2a13759e477782edb231ab8d8ee046c
mohaneie <mohaneie88@gmail.com>
2018-11-04 11:02:39 +0530

    change API is converted as async await

--- Routes/changepassword.js
+++ Routes/changepassword.js
@@ -1,28 +1,29 @@
 'use strict';
-const User = require('../Models/user');
+const { User } = require('../Models')
 const passwordserv = require('../helper/password');
 
 
 module.exports = function (app) {
     app.post('/update', async (req, res, next) => {
-        const { Email, Password } = req.body;
+        const {Email, Password} = req.body;
+        // console.log(Email, Password);
         try {
-            const data = await User.findOne({ Email })
-            if (!data) {
-                return next('email is not found')
+            const data = await User.findOne({Email})
+            if(!data) {
+                const error = new Error('Email is doesnt exit');
+                return next(error)
             }
-
-            const hashpass = await passwordserv.hash(Password)
-            req.body.Password = hashpass;
-            const some = new User(req.body);
-            const result = await some.save();
+            const hash = await passwordserv.hash(Password);
+            req.body.Password = hash;
+            const info = req.body;
+            const result = await User.findOneAndUpdate({Email}, info, {new:true});
             res.json(result);
 
         }
+         catch(error) {
+             next(error);
+         }
 
-        catch (error) {
-            next(error)
-        }
 
     })
 }
